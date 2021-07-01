398ecf8055a32f1d85f792f65aafc0b8fce588b0
RoccoCocco <rokokokeza@gmail.com>
2021-07-01 15:33:45 +0200

    feat: sentry implementation

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as Sentry from '@sentry/node';
 
 import { AppModule } from './app.module';
+import { SentryInterceptor } from './interceptors/sentry';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,7 +20,13 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/docs', app, document);
 
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    environment: process.env.ENVIRONMENT || 'local',
+  });
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new SentryInterceptor());
   await app.listen(process.env['PORT'] || 3000);
 }
 bootstrap();
